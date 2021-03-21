cdc5e11f9f7f8b809c185cfb1cb9336eddd2db07
Minatulaev Muhammad <minatulaev867@gmail.com>
2021-03-21 13:45:45 +0500

    rabota v lab-3status

--- src/chessviz/main.cpp
+++ src/chessviz/main.cpp
@@ -1,7 +1,7 @@
-#include "move.h"
-#include "print.h"
-
-
+#include <iostream>
+#include <libchessviz/move.h>
+#include <libchessviz/print.h>
+using namespace std;
 int main()
 {
     char chessboard[11][11] = {
