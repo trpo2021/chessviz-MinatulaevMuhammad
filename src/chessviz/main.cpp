1bfc84483462fe10c27d5dbe6c3989ff9b7bdf61
Minatulaev Muhammad <minatulaev867@gmail.com>
2021-03-27 10:28:36 +0500

    Automatic assembly

--- src/chessviz/main.cpp
+++ src/chessviz/main.cpp
@@ -1,22 +1,20 @@
 #include <iostream>
-#include <C:\Users\minat\chessviz-MinatulaevMuhammad\src\libchessviz/move.h>
-#include <C:\Users\minat\chessviz-MinatulaevMuhammad\src\libchessviz/print.h>
+#include <libchessviz/move.h>
+#include <libchessviz/print.h>
 using namespace std;
 int main()
 {
-    char chessboard[11][11] = {
-        "1rnbqkbnr",
-        "2pppppppp",
-        "3        ",
-        "4        ",
-        "5        ",
-        "6        ",
-        "7PPPPPPPP",
-        "8RNBQKBNR"
-    };
+    char chessboard[11][11]
+            = {"1rnbqkbnr",
+               "2pppppppp",
+               "3        ",
+               "4        ",
+               "5        ",
+               "6        ",
+               "7PPPPPPPP",
+               "8RNBQKBNR"};
     print(chessboard);
-   while (1) {
-   	    
+    while (1) {
         move(chessboard);
     }
     return 0;
--- src/libchessviz/print.cpp
+++ src/libchessviz/print.cpp
@@ -1,21 +1,20 @@
-#include <libchessviz/print.h>
 #include <iostream>
+#include <libchessviz/print.h>
 #include <stdio.h>
 using namespace std;
 
 void print(char chessboard[11][11])
 {
-	int i, z;
-	for (i = 0; i < 8; i++) {
-		for (z = 0; z < 9; z++) {
-			if (z == 1)
-				cout << chessboard[i][z];
-			else
-				cout << chessboard[i][z];
-		}
-		cout << "\n";
-	}
-	cout << "\nabcdefgh";
-	cout << "\n";
-
+    int i, z;
+    for (i = 0; i < 8; i++) {
+        for (z = 0; z < 9; z++) {
+            if (z == 1)
+                cout << chessboard[i][z];
+            else
+                cout << chessboard[i][z];
+        }
+        cout << "\n";
+    }
+    cout << "\nabcdefgh";
+    cout << "\n";
 }
