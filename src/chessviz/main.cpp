ac716bbe461cd4346dbd8d658b254a309cc68279
Minatulaev Muhammad <minatulaev867@gmail.com>
2021-04-30 21:58:40 +0500

    Returning functions

---
+++ src/chessviz/main.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+#include <libchessviz/move.h>
+#include <libchessviz/print.h>
+using namespace std;
+
+int main()
+{
+    char chessboard[11][11]
+            = {"1rnbqkbnr",
+               "2pppppppp",
+               "3        ",
+               "4        ",
+               "5        ",
+               "6        ",
+               "7PPPPPPPP",
+               "8RNBQKBNR"};
+    print(chessboard);
+    while (1) {
+        move(chessboard);
+    }
+    return 0;
+}
